e5c85df7b6b536ff1a83a42ba90f9271003947e5
rotariTudor <tudor.rotari007@gmail.com>
2025-04-07 19:11:49 +0300

    3rd commit

--- treasure_manager.c
+++ treasure_manager.c
@@ -27,10 +27,45 @@ typedef struct{
 }Hunt;
 
 
-void add(Treasure Tarray,char *huntID){
-    int folder = mkdir(huntID,S_IRWXU | S_IRWXG | S_IRWXO);
+void add(Treasure Tarray, char *huntID){
+
+    int check = chdir(huntID);
+    
+    if(check==-1){
+        printf("Directory-ul %s nu exista.\n",huntID);
+        int folderCreate = mkdir(huntID,S_IRWXU | S_IRWXG | S_IRWXO);
+
+        if(folderCreate==0){
+            printf("S-a creat directory-ul %s.\n",huntID);
+            chdir(huntID);
+        }
+    }
+    else if(check==0){
+        printf("S-a deschis directory-ul %s.\n",huntID);
+    }
+
+    int fileCreate = open(Tarray.treasure_id, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    printf("S-a creat fisierul %s.\n",Tarray.treasure_id);
+
+    write(fileCreate,&Tarray.treasure_id,sizeof(Tarray.treasure_id));
+
+    write(fileCreate,&Tarray.username,sizeof(strlen(Tarray.username)));
+
+    char tempCoord[20];
+    sprintf(tempCoord,"%f %f",Tarray.coordinates.latitude, Tarray.coordinates.longitude);
+    write(fileCreate,&tempCoord,sizeof(tempCoord));
+    
+    char temp[20];
+    sprintf(temp,"%d",Tarray.value);
+    write(fileCreate,&temp,sizeof(strlen(temp)));
+
+    write(fileCreate,&Tarray.clue,sizeof(strlen(Tarray.clue)));
+
+    close(fileCreate);
 }
 
 int main(){
+    Treasure arr={"treasure1","Tudor",{1.2,2.5},25,"clue"};
+    add(arr,"proiect");
     return 0;
 }
