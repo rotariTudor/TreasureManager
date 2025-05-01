b4d4ef1709761134d2f68627c9118c6ef50d37fb
rotariTudor <tudor.rotari007@gmail.com>
2025-05-01 18:22:39 +0300

    2nd phase beginning

---
+++ treasure_hub.c
@@ -0,0 +1,61 @@
+#include "treasure_manager.c"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <signal.h>
+
+u_int8_t monitor_on_off=0;
+pid_t monitorpid;
+
+void handle_sigchld(int signal){
+    int status;
+    wait(&status);
+    monitor_on_off=0;
+    printf("Monitorizare încheiată cu exit status %d.\n", WEXITSTATUS(status));
+}
+
+void handle_sigusr2(int signal){
+    printf("Semnal SIGUSR2 primit.\n");
+    usleep(200000);
+    exit(-1);
+}
+
+void start_monitor(){
+    int pid;
+
+    if(monitor_on_off==1){
+        printf("Monitorul deja rulează.\n");
+        return;
+    }
+
+    else{
+        if((pid=fork())<0){
+            printf("Nu s-a putut realiza crearea procesului.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    if(pid==0){
+        execl("./monitor","monitor",NULL);
+        printf("Eroare la execuție");
+        exit(EXIT_FAILURE);
+    }
+
+    if(pid>0){
+        monitor_on_off=1;
+        monitorpid=pid;
+
+        for(;;){
+            printf("Introdu o opțiune:\n1 - List hunts\n2 - List treasures\n3 - View treasure\n4 - Stop monitor\n5 - exit\n");
+
+        }
+    }
+}
+
