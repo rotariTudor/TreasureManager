ac2fc121a1c763d6a3f4d292ee15abe5587e1453
rotariTudor <tudor.rotari007@gmail.com>
2025-05-03 09:25:08 +0300

    2nd phase -- DONE

--- treasure_hub.c
+++ treasure_hub.c
@@ -1,4 +1,5 @@
 #define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE 700
 #include "treasure_manager.h"
 #include <stdio.h>
 #include <sys/stat.h>
@@ -19,51 +20,47 @@ u_int8_t procfin = 0;
 
 void handle_sigchld(int signal){
     int status;
-    while (waitpid(-1, &status, WNOHANG) > 0) {
-        monitor_on_off = 0;
-        printf("Monitorizare încheiată cu exit status %d.\n", WEXITSTATUS(status));
-    }
+    wait(&status);
+    monitor_on_off=0;
+    printf("Monitor oprit cu exit status: %d.\n",WEXITSTATUS(status));
 }
 
 void handle_sigusr2(int signal){
-    printf("Semnal SIGUSR2 primit.\n");
-    exit(-1);
+    printf("Semnal SIGUSR2 de oprire a fost primit.\n");
+    sleep(3);
+    exit(0);
 }
 
 void start_monitor(){
     int pid;
     int opt;
 
-    if(monitor_on_off == 1){
-        printf("Monitorul deja rulează.\n");
-        return;
-    }
-    signal(SIGCHLD, handle_sigchld);
+    // if(monitor_on_off == 1){
+    //     printf("Monitorul deja rulează.\n");
+    //     return;
+    // }
+
     if((pid = fork()) < 0){
         printf("[FORK] Nu s-a putut realiza crearea procesului.\n");
         exit(EXIT_FAILURE);
     }
 
-    if(pid == 0){
-        if(execl("./monitor", "monitor", NULL)==-1){
-            printf("Eroare la execuție");
-            exit(EXIT_FAILURE);
-        }
-    }
-
     if(pid > 0){
         monitor_on_off = 1;
         monitorpid = pid;
-
-        signal(SIGCHLD, handle_sigchld);
+        printf("S-a creat un proces cu ID-ul: %d.\n",monitorpid);
+        printf("***************************\n");
 
         for(;;){
             printf("Introdu o opțiune:\n1 - List hunts\n2 - List treasures\n3 - View treasure\n4 - Stop monitor\n5 - exit\n");
+            printf("***************************\n");
             scanf("%d", &opt);
+            getchar();
             switch(opt){
                 case 1:
-                    printf("Toate hunturile din directory-ul cu hunturi.\n");
+                    printf("Toate hunturile din directory-ul cu hunturi:\n");
                     list_hunts();
+                    printf("***************************\n");
                     break;
 
                 case 2:
@@ -71,6 +68,7 @@ void start_monitor(){
                     char temp[15] = "";
                     scanf("%14s", temp);
                     list(temp);
+                    printf("***************************\n");
                     break;
 
                 case 3:
@@ -82,23 +80,27 @@ void start_monitor(){
                     char temp2[15] = "";
                     scanf("%14s", temp2);
                     view(temp1, temp2);
+                    printf("***************************\n");
                     break;
 
                 case 4:
                     if(monitor_on_off == 0){
                         printf("Monitorul deja este oprit.\n");
+                        printf("***************************\n");
                     } else {
                         printf("Monitorul se opreste...\n");
                         monitor_on_off = 0;
-                        sleep(3);
+                        sleep(3); // am folosit sleep deoarece nu-mi merge usleep, include-urile de mai sus tot au fost folosit pt a nu primi warning-uri in terminal la usleep dar nu a functionat
                         kill(monitorpid, SIGUSR2);
                         printf("Monitor oprit.\n");
+                        printf("***************************\n");
                     }
                     break;
 
                 case 5:
                     if(monitor_on_off == 1){
                         printf("Monitorul inca ruleaza.\n");
+                        printf("***************************\n");
                     } else {
                         procfin = 1;
                         exit(0);
--- treasure_manager.c
+++ treasure_manager.c
@@ -47,7 +47,8 @@ treasure exemple[10] = {
 };
 
 void addLog(char *huntID, char *mes){
-    
+    int count=0;
+
     char cwd[128]="";
     getcwd(cwd,sizeof(cwd));
     dirname(cwd);
@@ -86,10 +87,11 @@ void addLog(char *huntID, char *mes){
         unlink(symlinkName);
     }
     chdir("..");
-    if (symlink(logPath, symlinkName) == -1) {
+    if (symlink(logPath, symlinkName) == -1 && count == 0) {
         perror("Eroare la crearea symlink-ului, sau el deja exista.\n");
     } else {
         printf("Symlink creat: %s -> %s\n", symlinkName, logPath);
+        count=1;
     }
 }
 
@@ -351,86 +353,3 @@ void list_hunts(){
 
     closedir(cwd);
 }
-
-
-// int main(int argc, char *argv[]){
-
-//     if(argc<2){
-//         printf("Prea putine arg in linia de comanda.\n");
-//         return 1;
-//     }
-
-//     Option optiune;
-
-//     if(!strcmp(argv[1],"--add")) optiune=ADD_TREASURE;
-//     else if(!strcmp(argv[1],"--list_t")) optiune=LIST_TREASURE;
-//     else if(!strcmp(argv[1],"--view")) optiune=VIEW_TREASURE;
-//     else if(!strcmp(argv[1],"--remove_t")) optiune=REMOVE_TREASURE;
-//     else if(!strcmp(argv[1],"--remove_h")) optiune=REMOVE_HUNT;
-//     else if(!strcmp(argv[1],"--list_h")) optiune=LIST_HUNTS;
-//     else{
-//         printf("comanda necunoscuta.\nintrodu --add, --list_t, --view, --remove_t, --remove_h, -- list,h\n");
-//         return 1;
-//     }
-
-//     switch(optiune){
-//         case ADD_TREASURE:
-//             if(argc!=4){
-//                 perror("Introdu 4 argumente.\n");
-//                 exit(-1);
-//             }
-//             for(int i=0;i<10;i++){
-//                 if(!strcmp(exemple[i].treasure_id,argv[3])){
-//                     add(argv[2],exemple[i]);
-//                     return 0;
-//                 }
-//             }
-//             printf("Introdu un treasure existent (%s - %s).\n",exemple[0].treasure_id,exemple[9].treasure_id);
-//             break;
-
-//         case LIST_TREASURE:
-//             if(argc!=3){
-//                 perror("Introdu 3 argumente pentru list treasure.\n");
-//                 exit(-2);
-//             }
-//             list(argv[2]);
-//             break;
-
-//         case VIEW_TREASURE:
-//             if(argc!=4){
-//                 perror("Introdu 4 argumente pentru view treasure.\n");
-//                 exit(-3);
-//             }
-//             view(argv[2],argv[3]);
-//             break;
-
-//         case REMOVE_TREASURE:
-//             if(argc!=4){
-//                 perror("Introdu 4 argumente pentru remove treasure.\n");
-//                 exit(-4);
-//             }
-//         remTreasure(argv[2],argv[3]);
-//         break;
-
-//         case REMOVE_HUNT:
-//         if(argc!=3){
-//             perror("Introdu 3 argumente pentru remove hunt.\n");
-//             exit(-5);
-//         }
-//         remHunt(argv[2]);
-//         break;
-
-//         case LIST_HUNTS:
-//             if(argc!=2){
-//                 perror("Introdu 2 argumente pentru list hunt.\n");
-//                 exit(-6);
-//             }
-//             list_hunts();
-//             break;
-
-//         default:
-//             printf("Introdu o optiune valida!.\n");
-//             break;
-//     }
-//     return 0;
-// }
